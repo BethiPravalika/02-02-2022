1bbe8cab91c0159c5cada1b10bed1e8cc50b5a89
Pravalika <Pravalika@DESKTOP-CJIU4FR>
2022-02-03 14:52:33 +0530

    secondproject javacomments with program in 02-02-2022

---
+++ 02-02-2022/src/com/javacomments/Example2.java
@@ -0,0 +1,17 @@
+package com.javacomments;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		System.out.println("bethipravalika2222@gmail.com");
+		
+		
+		//singl line comments
+		/**   Multiline comments
+		 * *
+		 * *
+		* */
+
+	}
+
+}
